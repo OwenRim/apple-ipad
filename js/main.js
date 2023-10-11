f1323593d1cdeeb4ca4e425b9a111f965ad134f2
owen <owen.rim@edwardsvacuum.com>
2023-10-11 15:11:13 +0900

    38.Compare 섹션

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+import ipads from '../data/ipads.js'
+
 // 장바구니!
 // 장바구니 관련 요소 찾기.
 const basketStarterEl = document.querySelector('header .basket-starter')
@@ -111,4 +113,33 @@ pauseBtn.addEventListener('click', function () {
     video.pause()
     playBtn.classList.remove('hide')
     pauseBtn.classList.add('hide')
+})
+
+// '당신에게 맞는 iPad는?' 랜더링!
+const itemsEl = document.querySelector('section.compare .items')
+ipads.forEach(ipad => {
+    const itemEl = document.createElement('div')
+    itemEl.classList.add('item')
+
+    let colorList = ''
+    ipad.colors.forEach(color => {
+        colorList += `<li style="background-color: ${color};"></li>`
+    })
+
+    // VS Code 확장 프로그램 - Comment tagged templates
+    itemEl.innerHTML = /* html */ `
+    <div class="thumbnail">
+      <img src="${ipad.thumbnail}" alt="${ipad.name}" />
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">₩${ipad.price.toLocaleString('en-US')}부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+  `
+
+    itemsEl.append(itemEl)
 })
